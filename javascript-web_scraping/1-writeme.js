ffe37c02184e96fafdea609ff0ee5b8e6ac50c0d
prodjohnper <JonathanPerez9743@gmail.com>
2024-05-13 22:34:48 -0400

    Add - Created script that writes a string to a file

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -5,11 +5,13 @@ const fs = require('fs');
 const filePath = process.argv[2];
 const content = process.argv[3];
 
+// If no file path is provided, show an error and return
 if (!filePath) {
   console.error('Please provide a file path');
   process.exit(1);
 }
 
+// If no content is provided, show an error and return
 if (!content) {
   console.error('Please provide content to write');
   process.exit(1);
