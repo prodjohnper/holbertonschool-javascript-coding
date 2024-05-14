0f2a8fb9a94bc1a9f6fa5854807bbbb52f77fc60
prodjohnper <JonathanPerez9743@gmail.com>
2024-05-14 00:24:34 -0400

    Add - Created script that prints the title of a Star Wars movie where the episode number matches a given int

---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,39 @@
+#!/usr/bin/node
+
+// Import the request library
+const request = require('request');
+
+// Get the movie ID from the command line arguments
+const movieId = process.argv[2];
+const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
+
+if (!movieId) {
+  console.error('Please provide a movie ID');
+  process.exit(1);
+}
+
+// Make a request to the URL and get the movie title
+request(apiUrl, (err, response, body) => {
+  if (err) {
+    // Print the error if one occurred
+    console.error('Error:', err);
+    return;
+  }
+
+  // Check if the response status code is not 200
+  if (response.statusCode !== 200) {
+    // Print an error message if the status code is not 200
+    console.error('Failed to fetch data:', response.statusCode);
+    return;
+  }
+
+  // Parse the JSON response
+  try {
+    const movie = JSON.parse(body);
+    // Print the movie title
+    console.log('Title:', movie.title);
+  } catch (parseError) {
+    // Print an error if the JSON parsing fails
+    console.error('Error parsing JSON:', parseError);
+  }
+});
