3a6fab43c9700603a6ac70593943a7130a6b6b44
prodjohnper <JonathanPerez9743@gmail.com>
2024-05-14 11:20:30 -0400

    Add - Created script that gets the content of a webpage and stores it in a file

---
+++ javascript-web_scraping/5-request_store.js
@@ -0,0 +1,38 @@
+#!/usr/bin/node
+
+// Import the request module
+const request = require('request');
+const fs = require('fs');
+
+// Get the API URL and file name from the command line args
+const apiUrl = process.argv[2];
+const fileName = process.argv[3];
+
+// Check if the API URL and file name are provided
+if (!apiUrl) {
+  console.error('Please provide a valid API URL');
+}
+
+// Check if the file name is provided
+if (!fileName) {
+  console.error('Please provide a file name');
+}
+
+// Make a request to the API
+request(apiUrl, 'utf-8', (err, response, body) => {
+  // Check if there is an error
+  if (err) {
+    // Print the error
+    console.error(err);
+  } else {
+    // Write the response to a file
+    fs.writeFile(fileName, body, (err) => {
+      if (err) {
+        // Print the error
+        console.error(err.message);
+      }
+      // Print that the file was saved
+      console.log('File saved');
+    });
+  }
+});
