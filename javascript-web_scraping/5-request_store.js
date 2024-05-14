bb936e1382a56c9f1cd4c25787325753a31b047e
prodjohnper <JonathanPerez9743@gmail.com>
2024-05-14 11:43:52 -0400

    Add - Created script that gets the content of a webpage and stores it in a file

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -23,20 +23,18 @@ request(apiUrl, (err, response, body) => {
   // Check if there is an error
   if (err) {
     // Print the error
-    console.error(err);
+    console.error(err.message);
   }
 
   // Check if the status code is not 200
   if (response.statusCode !== 200) {
-    console.error(`Invalid status code <${response.statusCode}>`);
+    console.error(`Invalid status code ${response.statusCode}`);
   }
   // Write the response to a file
-  fs.writeFile(fileName, body, { encoding: 'utf-8' }, (err) => {
+  fs.writeFile(fileName, body, 'utf-8', (err) => {
     if (err) {
       // Print the error
       console.error(err.message);
     }
-    // Print that the file was saved
-    console.log(`File saved succesfully to ${fileName}`);
   });
 });
