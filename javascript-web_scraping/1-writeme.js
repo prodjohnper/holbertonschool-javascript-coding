75957ccac3a6b8f3437b002c1660d72be77fdba5
prodjohnper <JonathanPerez9743@gmail.com>
2024-05-13 22:33:04 -0400

    Add - Created script that writes a string to a file

---
+++ javascript-web_scraping/1-writeme.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+const filePath = process.argv[2];
+const content = process.argv[3];
+
+if (!filePath) {
+  console.error('Please provide a file path');
+  process.exit(1);
+}
+
+if (!content) {
+  console.error('Please provide content to write');
+  process.exit(1);
+}
+
+// Write the content to the file
+fs.writeFile(filePath, content, 'utf-8', (err) => {
+  // If an error occurred, show it and return
+  if (err) {
+    console.error(err);
+    return;
+  }
+  // If no error occurred, show a success message
+  console.log('File has been written successfully!');
+});
