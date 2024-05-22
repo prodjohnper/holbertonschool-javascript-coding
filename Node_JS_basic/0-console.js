4e2e7f89fa3ff0ab12322c6deb85531a4e51300e
prodjohnper <JonathanPerez9743@gmail.com>
2024-05-22 17:16:50 -0400

    Fix - Fixed output error. Refactored exporting

--- Node_JS_basic/0-console.js
+++ Node_JS_basic/0-console.js
@@ -4,11 +4,13 @@
   Function that prints in `STDOUT` the str argument.
 */
 
-module.exports = function displayMessage(str) {
+function displayMessage(str) {
   if (!str) {
     // If the str argument is not provided, throw an error
     throw new Error('The string argument is required');
   }
   // Print the message in the `STDOUT`
   console.log(str);
-};
+}
+// Export the function to be used in other files
+module.exports = displayMessage;
