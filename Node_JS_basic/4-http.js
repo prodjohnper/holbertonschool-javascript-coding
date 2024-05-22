ac0c91de6a07c07d4295c2137534167ade7b76ae
prodjohnper <JonathanPerez9743@gmail.com>
2024-05-22 19:37:32 -0400

    Add - Created a small HTTP server using http module

---
+++ Node_JS_basic/4-http.js
@@ -0,0 +1,28 @@
+/*
+  4-http.js
+
+  Function that creates a server that listens on port
+  1245 and returns a message depending on the request.
+*/
+
+// Import the HTTP module
+const http = require('http');
+
+// Create the HTTP server
+const app = http.createServer((req, res) => {
+  // Set the response HTTP header
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+
+  // Send the response body
+  res.end('Hello Holberton School!');
+});
+
+// Set server's port
+app.listen(1245, () => {
+  // Log the server's port
+  console.log('Server is listening on port 1245');
+});
+
+// Export the app variable
+module.exports = app;
