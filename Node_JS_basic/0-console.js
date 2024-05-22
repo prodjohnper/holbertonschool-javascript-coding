ac0fb4b0bf786e83e2c6632bd230b6d6a9ada237
prodjohnper <JonathanPerez9743@gmail.com>
2024-05-22 17:39:26 -0400

    Fix - Fixed output error. Un-implemented arrow function

--- Node_JS_basic/0-console.js
+++ Node_JS_basic/0-console.js
@@ -4,13 +4,9 @@
   Function that prints in `STDOUT` the str argument.
 */
 
-const displayMessage = (string) => {
-  if (!string) {
-    // If the str argument is not provided, throw an error
-    throw new Error('The string argument is required');
-  }
-  // Print the message in the `STDOUT`
-  console.log(string);
-};
+function displayMessage(str) {
+  console.log(str);
+}
+
 // Export the function to be used in other files
 module.exports = displayMessage;
