c61880e592b46ac75c5d86205706178ecdd2ca92
prodjohnper <JonathanPerez9743@gmail.com>
2024-05-22 18:07:42 -0400

    Add - Created program that is executed thru command line and gets user input

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -3,20 +3,19 @@
 
   Program that executes through command line and waits for user input.
 */
-const process = require('process');
 
 // Display welcome msg
 console.log('Welcome to Holberton School, what is your name?');
 
-// Get the user's input
-process.stdin.on('data', (input) => {
-  // Trim whitespaces or newlines
-  const name = input.toString().trim();
+// Get user input
+process.stdin.on('readable', () => {
+  // Read user input
+  const input = process.stdin.read();
+  // If data is not null, display user input
+  if (input !== null) process.stdout.write(`Your name is: ${input.toString()}`);
+});
 
-  // Display the user's name
-  console.log(`Your name is: ${name}`);
-
-  // Display the closing message and exit the program
-  console.log('This important software is now closing');
-  process.exit();
+// Display closing msg
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
