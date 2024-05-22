73224d3364477cbdf43c2ebe44e853217fdf16bb
prodjohnper <JonathanPerez9743@gmail.com>
2024-05-22 17:41:54 -0400

    Fix - Fixed output error. Implemented arrow function

--- Node_JS_basic/0-console.js
+++ Node_JS_basic/0-console.js
@@ -4,9 +4,9 @@
   Function that prints in `STDOUT` the str argument.
 */
 
-function displayMessage(string) {
-  console.log(string);
-}
+const displayMessage = (str) => {
+  console.log(str);
+};
 
 // Export the function to be used in other files
 module.exports = displayMessage;
