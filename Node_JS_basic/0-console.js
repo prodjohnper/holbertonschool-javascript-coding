034cf885faacc1b478eda6fd81b969aabe5630d0
prodjohnper <JonathanPerez9743@gmail.com>
2024-05-22 17:11:35 -0400

    Add - Created function that prints a str argument to the `STDOUT`

---
+++ Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/*
+  0-console.js
+
+  Function that prints in `STDOUT` the str argument.
+*/
+
+module.exports = function displayMessage(str) {
+  // Print the message in the `STDOUT`
+  console.log(str);
+};
