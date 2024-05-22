b26ef9acca31d9390b9c11bba3dd0832b19a1e36
prodjohnper <JonathanPerez9743@gmail.com>
2024-05-22 17:14:02 -0400

    Fix - Fixed output error. Implemented error case handling

--- Node_JS_basic/0-console.js
+++ Node_JS_basic/0-console.js
@@ -5,6 +5,10 @@
 */
 
 module.exports = function displayMessage(str) {
+  if (!str) {
+    // If the str argument is not provided, throw an error
+    throw new Error('The string argument is required');
+  }
   // Print the message in the `STDOUT`
   console.log(str);
 };
