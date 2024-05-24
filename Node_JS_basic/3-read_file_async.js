3a1ead4558c1674e7cd912d4b6f0f38e9cbe3b72
prodjohnper <JonathanPerez9743@gmail.com>
2024-05-24 11:03:18 -0400

    Fix

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -7,66 +7,56 @@
 */
 
 // Import the filesystem module
-const fs = require('fs');
+const fs = require('fs').promises;
 
-function countStudents(path) {
-  // Return a promise
-  return new Promise((resolve, reject) => {
-    // Read the file
-    fs.readFile(path,
-      { encoding: 'utf8', flag: 'r' },
-      (err, data) => {
-        // If the file does not exist or cannot be read return an error
-        if (err) {
-          // Reject the promise
-          reject(Error('Cannot load the database'));
-          return;
+// Function that counts the number of students in a database
+const countStudents = (filePath) => new Promise((resolve, reject) => {
+  // Read the file content, and split it by lines
+  fs.readFile(filePath, 'utf8')
+    .then((data) => {
+      const lines = data.trim().split('\n');
+      const students = lines.slice(1).filter((line) => line.trim() !== '');
+      // Print the number of students
+      console.log(`Number of students: ${students.length}`);
+
+      // Create an object to store the number of students per field
+      const fields = {};
+      students.forEach((student) => {
+        // Split the student by commas
+        const [firstName, , , field] = student.split(',');
+        if (fields[field]) {
+          // If the field already exists, increment the count
+          fields[field].count += 1;
+          fields[field].students.push(firstName);
+        } else {
+          // If the field does not exist, create it
+          fields[field] = {
+            count: 1,
+            students: [firstName],
+          };
         }
-        // Split the data by new line
-        const response = [];
-        let message;
-
-        // Remove the last line (empty line) and split the data by commas
-        const content = data.split('\n');
-        let students = content.filter((item) => item);
-        students = students.map((item) => item.split(','));
-
-        // Get the number of students and the fields they are in
-        const studentSize = students.length ? students.length - 1 : 0;
-        message = `Number of students: ${studentSize}`;
-        console.log(message);
-
-        // Add the message to the response
-        response.push(message);
-
-        // Create an obj to store the fields and the students in each field
-        const fields = {};
-        for (const i in students) {
-          // Skip the first line
-          if (i !== 0) {
-            // If the field does not exist, create it
-            if (!fields[students[i][3]]) fields[students[i][3]] = [];
-            fields[students[i][3]].push(students[i][0]);
-          }
-        }
-
-        // Remove the field key from the obj
-        delete fields.field;
-
-        // Get the number of students in each field
-        for (const key of Object.keys(fields)) {
-          // Get the number of students in each field
-          message = `Number of students in ${key}: ${fields[key].length
-          }. List: ${fields[key].join(', ')}`;
-
-          console.log(message);
-
-          response.push(message);
-        }
-        // Resolve the promise
-        resolve(response);
       });
-  });
-}
+
+      // Print the number of students per field
+      Object.entries(fields).forEach(([field, { count, students }]) => {
+        console.log(
+          `Number of students in ${field}: ${count}. List: ${students.join(
+            ', ',
+          )}`,
+        );
+      });
+
+      // Resolve the promise
+      resolve();
+    })
+    .catch((err) => {
+      // If an error occurs, reject the promise with the error
+      if (err.code === 'ENOENT') {
+        reject(new Error('Cannot load the database'));
+      } else {
+        reject(err);
+      }
+    });
+});
 
 module.exports = countStudents;
